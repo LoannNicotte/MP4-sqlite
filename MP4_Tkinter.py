12fb634a3b58502fb7bbdd204d4c4658854d4928
LoannNicotte <65536418+LoannNicotte@users.noreply.github.com>
2020-10-30 15:59:07 +0100

    Add files via upload

--- MP4_Tkinter.py
+++ MP4_Tkinter.py
@@ -4,6 +4,18 @@ from tkinter.ttk import *
 
 #---------------------------------------------------------------------------------------------------------------------------  
 
+def menu():
+    global fenetre
+    fenetre = Tk()
+
+    Button(fenetre, text="AJOUT", command=choix1).pack(pady = 5, fill = X)
+    Button(fenetre, text="SUPRESSION", command=choix2).pack(pady = 5, fill = X)
+    Button(fenetre, text="MODIFICATION", command=choix3).pack(pady = 5, fill = X)
+    Button(fenetre, text="VISUALISATION", command=choix4).pack(pady = 5, fill = X)
+    Button(fenetre, text="EXIT", command=Exit).pack(pady = 5, fill = X)
+    
+    fenetre.mainloop()
+
 def choix1():
     global choix
     quitter()
@@ -20,17 +32,18 @@ def choix3():
     choix = 3
 
 def choix4():
-    global choix
+    global choix, fenetre
     quitter()
     choix = 4
 
 def Exit():
-    global choix
+    global choix, fenetre
     fenetre.destroy()
     fenetre.quit()
     choix = 5
     
 def quitter():
+    global fenetre
     fenetre.destroy()
     fenetre.quit()
 
@@ -40,6 +53,27 @@ def conectBDD():
     cur.execute("""CREATE TABLE IF NOT EXISTS ANNUAIRE
                     (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, nom TEXT, prenom TECT, tel INT,
                     email TEXT, qualité TEXT)""")
+    
+def donneBD():
+    global BD
+    cur.execute('SELECT * FROM ANNUAIRE')
+    conn.commit()
+    BD = cur.fetchall()
+    
+def afficherBD():
+    global BD
+    tableau = Treeview(fenetre, columns=('ID', 'NOM', 'PRENOM', 'TELEPHONE', 'EMAIL', 'QUALITE'))
+    tableau.heading('ID', text='ID')
+    tableau.heading('NOM', text='NOM')
+    tableau.heading('PRENOM', text='PRENOM')
+    tableau.heading('TELEPHONE', text='TELEPHONE')
+    tableau.heading('EMAIL', text='EMAIL')
+    tableau.heading('QUALITE', text='QUALITE')
+    tableau['show'] = 'headings' # sans ceci, il y avait une colonne vide à gauche qui a pour rôle d'afficher le paramètre "text" qui peut être spécifié lors du insert
+    tableau.pack(padx = 5, pady = (0, 5))
+    
+    for i in range(len(BD)):
+        tableau.insert('', 'end', iid=i+1, values=(i+1, BD[i][1], BD[i][2], BD[i][3], BD[i][4], BD[i][5]))
   
 def ajouterBDD():
     
@@ -48,42 +82,29 @@ def ajouterBDD():
     cur.execute("INSERT INTO ANNUAIRE(nom,prenom,tel,email,qualité) VALUES(?, ?, ?, ?, ?)" , datas)
     conn.commit()
     
-    fenetre.destroy()
-    fenetre.quit()
+    quitter()
 
 def suprBDD():
     
-    cur.execute('SELECT * FROM ANNUAIRE')
-    conn.commit()
-    BD = cur.fetchall()        
+    donneBD()        
     
-    nom = BD[int(supr.get())-1][1]
-    prenom = BD[int(supr.get())-1][2]
-    
-    suppr = (nom,prenom)
+    suppr = (BD[int(supr.get())-1][1],BD[int(supr.get())-1][2])
         
     cur.execute('DELETE FROM ANNUAIRE WHERE nom = ? AND prenom = ?', suppr)
     conn.commit()
     
-    fenetre.destroy()
-    fenetre.quit()
-
-def modifBDD(colone, colone_connu, ligne, modif) :
-
-    modif = (modif, ligne)
+    quitter()
     
-    cur.execute('UPDATE ANNUAIRE SET {} = ? WHERE {} = ?'.format(colone, colone_connu), modif)
-    conn.commit()
 
-def rechBDD(colone, colone_cond, ligne_cond, cond):
+def modifBDD() :
+    donneBD()
+
+    change = (modif.get(), BD[int(modifID.get())-1][1], BD[int(modifID.get())-1][2])
     
-    if cond == "oui":
-        ligne_cond = (ligne_cond,)
-        cur.execute('SELECT {} FROM ANNUAIRE WHERE {} = ?'.format(colone, colone_cond), (ligne_cond))
-    else:
-        cur.execute('SELECT {} FROM ANNUAIRE'.format(colone))
+    cur.execute('UPDATE ANNUAIRE SET {} = ? WHERE nom = ? AND prenom = ?'.format(modifAT.get()), change)
     conn.commit()
-    print('\n',cur.fetchall())
+    
+    quitter()
 
 
 conn = sqlite3.connect('Annuaire.db')
@@ -92,23 +113,15 @@ cur.execute("""CREATE TABLE IF NOT EXISTS ANNUAIRE
                 (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, nom TEXT, prenom TECT, tel INT,
                 email TEXT, qualité TEXT)""")
 
-fenetre = Tk()
-
-Button(fenetre, text="AJOUT", command=choix1).pack(pady = 5)
-Button(fenetre, text="SUPRESSION", command=choix2).pack(pady = 5)
-Button(fenetre, text="MODIFICATION", command=choix3).pack(pady = 5)
-Button(fenetre, text="BDD", command=choix4).pack(pady = 5)
-Button(fenetre, text="EXIT", command=Exit).pack(pady = 5)
-
-fenetre.mainloop()
+menu()
 while choix != 5:
     if choix == 1:
        
         fenetre = Tk()
         
-        Button(fenetre, text="EXIT", command=Exit).pack(side = TOP, fill = X, pady = 5)
+        Button(fenetre, text="EXIT", command=Exit, bg = '#B0FCFD').pack(side = TOP, fill = X, pady = 5)
 
-        Label(fenetre, text= 'Ajout dans la base de donnée').pack(side=TOP, pady=10)
+        Label(fenetre, text= 'Ajout à la base de donnée')
         
         Label(fenetre, text = 'Nom (string) :').pack(side=TOP, padx=5, pady=5)
         nom = Entry(fenetre, width=30)
@@ -139,86 +152,93 @@ while choix != 5:
     elif choix == 2:
         
         ID = []
+        
+        donneBD()
 
         fenetre = Tk()
         
         Button(fenetre, text="EXIT", command=Exit).pack(side = TOP, fill = X, pady = 5)
         
-        tableau = Treeview(fenetre, columns=('ID', 'NOM', 'PRENOM', 'TELEPHONE', 'EMAIL', 'QUALITE'))
-        tableau.heading('ID', text='ID')
-        tableau.heading('NOM', text='NOM')
-        tableau.heading('PRENOM', text='PRENOM')
-        tableau.heading('TELEPHONE', text='TELEPHONE')
-        tableau.heading('EMAIL', text='EMAIL')
-        tableau.heading('QUALITE', text='QUALITE')
-        tableau['show'] = 'headings' 
-        tableau.pack(padx = 5, pady = (0, 5))
+        Label(fenetre, text= 'Suprimer de la base de donnée').pack(side=TOP, pady=10)
+        
+        afficherBD()
         
         for i in range(len(BD)):
-            tableau.insert('', 'end', iid=i, values=(i+1, BD[i][1], BD[i][2], BD[i][3], BD[i][4], BD[i][5]))
             ID.append(i+1)
         
         Label(fenetre, text= 'Que suprimer (ID) ?').pack(pady=5)
 
         supr = Combobox(fenetre, values=ID)
         supr.current(0)
-        supr.pack()
+        supr.pack(pady=5)
         
-        Button(fenetre, text="Suprimer", command=suprBDD).pack()
+        Button(fenetre, text="Suprimer", command=suprBDD).pack(pady=5)
         
-        Button(fenetre, text="Fermer", command=quitter).pack()
+        Button(fenetre, text="Fermer", command=quitter).pack(pady=5)
                
         fenetre.mainloop()        
 
     elif choix == 3:
-        colone = input("Quelle colone modifier ?\n"
-                        "(nom, prenom, tel ,email, qualité)\n")
-        colone_connu = input("De quelle colone est la donnée connue ?\n"
-                      "(nom, prenom, tel ,email, qualité)\n")
-        ligne = input("Quelle est la donnée connu de {} ?\n".format(colone_connu))
-        modif = input("Avec quelle donnée changer {} ? \n".format(colone))
-        modifBDD(colone, colone_connu, ligne, modif)        
-
-    elif choix == 4:
-
-        cur.execute('SELECT * FROM ANNUAIRE')
-        conn.commit()
-        BD = cur.fetchall()
-        print(BD)
-
+        
+        ID = []
+        
+        donneBD()
 
         fenetre = Tk()
         
         Button(fenetre, text="EXIT", command=Exit).pack(side = TOP, fill = X, pady = 5)
-
-        tableau = Treeview(fenetre, columns=('NOM', 'PRENOM', 'TELEPHONE', 'EMAIL', 'QUALITE'))
-        tableau.heading('NOM', text='NOM')
-        tableau.heading('PRENOM', text='PRENOM')
-        tableau.heading('TELEPHONE', text='TELEPHONE')
-        tableau.heading('EMAIL', text='EMAIL')
-        tableau.heading('QUALITE', text='QUALITE')
-        tableau['show'] = 'headings' # sans ceci, il y avait une colonne vide à gauche qui a pour rôle d'afficher le paramètre "text" qui peut être spécifié lors du insert
-        tableau.pack(padx = 5, pady = (0, 5))
+        
+        Label(fenetre, text= 'Modifier la base de donnée').pack(side=TOP, pady=10)
+        
+        afficherBD()
         
         for i in range(len(BD)):
-            tableau.insert('', 'end', iid=i+1, values=(BD[i][1], BD[i][2], BD[i][3], BD[i][4], BD[i][5]))
+            ID.append(i+1)
         
-        bouton=Button(fenetre, text="Fermer", command=quitter).pack()
+        Label(fenetre, text= 'Que modifier (ID) ?').pack(pady=5)
+
+        modifID = Combobox(fenetre, values=ID)
+        modifID.current(0)
+        modifID.pack()
+        
+        Label(fenetre, text= 'Quel atribut modifier ?').pack(pady=5)
+        
+        modifAT = Combobox(fenetre, values=['NOM', 'PRENOM', 'TELEPHONE', 'EMAIL', 'QUALITE'])
+        modifAT.current(0)
+        modifAT.pack()
+        
+        Label(fenetre, text= 'Par quelle valeur changer cet atribut ?').pack(pady=5)
+        
+        modif = Entry(fenetre)
+        modif.pack(pady=5)
+        
+        Button(fenetre, text="Modifier", command=modifBDD).pack(pady=5)
+        
+        Button(fenetre, text="Fermer", command=quitter).pack(pady=5)
+               
+        fenetre.mainloop()        
+
+    elif choix == 4:
+
+        donneBD()
+
+        fenetre = Tk()
+        
+        Button(fenetre, text="EXIT", command=Exit).pack(side = TOP, fill = X, pady = 5)
+        
+        Label(fenetre, text= 'Visualisation de la base de donnée').pack(side=TOP, pady=10)
+
+        afficherBD()
+        
+        bouton=Button(fenetre, text="Fermer", command=quitter).pack(pady=5)
 
         fenetre.mainloop()
         
     if choix != 5:          
-        fenetre = Tk()
-
-        Button(fenetre, text="AJOUT", command=choix1).pack(pady = 5)
-        Button(fenetre, text="SUPRESSION", command=choix2).pack(pady = 5)
-        Button(fenetre, text="MODIFICATION", command=choix3).pack(pady = 5)
-        Button(fenetre, text="BDD", command=choix4).pack(pady = 5)
-        Button(fenetre, text="EXIT", command=Exit).pack(pady = 5)
-        
-        fenetre.mainloop() 
+        menu() 
        
 
 cur.close()
 conn.close()
 print("\nFin du programme")
+#FIN
